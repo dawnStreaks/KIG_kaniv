d43f9bc553b594980cf737356797703df99a8958
dawnStreaks <sabeehakpm@gmail.com>
2025-11-22 13:48:06 +0300

    changes to bar chart showing numbers

--- resources/views/reports/collection.blade.php
+++ resources/views/reports/collection.blade.php
@@ -106,7 +106,10 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
+    <script>
+        Chart.register(ChartDataLabels);
+    </script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const filterInputs = document.querySelectorAll('.filter-input');
